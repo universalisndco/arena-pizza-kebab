a49ad954781123636fac1fcc9493476e011717a9
universalisndco <universalis.ndco@gmail.com>
2026-06-06 18:35:02 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,8 +3,8 @@
 
 var firebaseConfig = {
   apiKey: "AIzaSyCaxE-POSptcvcXAGSYq7PFKPMcMnRFouM",
-  authDomain: "arena-pizza-kebab-8e281.firebaseapp.com",
-  projectId: "arena-pizza-kebab-8e281",
+  authDomain: "arena-pizza-kebab-3f078.firebaseapp.com",
+  projectId: "arena-pizza-kebab-3f078",
   storageBucket: "arena-pizza-kebab-8e281.firebasestorage.app",
   messagingSenderId: "882481455580",
   appId: "1:882481455580:web:2f17aa7e76bae740de2f19"
